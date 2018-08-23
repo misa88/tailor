9d4543d857c3b967966bbfc88c1e60e11ea04e76
EC2 Default User <ec2-user@ip-172-31-18-239.ap-southeast-1.compute.internal>
2018-08-23 22:08:23 +0000

    new files

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def top
+  end
+  
+  def top_recruit
+  end
+  
+  def login_form
+  end
+  
+  def mypage
+  end
+  
+  def login
+      @user = User.find_by(email: params[:mail], password: params[:pass])
+      if @user
+          flash[:notice] = "ログインしました"
+          redirect_to("/mypage")
+      else
+        @error_message = "メールアドレスまたはパスワードが間違っています"
+        render("users/login_form")
+      end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'users#top'
+  get '/top_recruit' => 'users#top_recruit'
+  get '/login' => 'users#login_form'
+  get '/mypage' => 'users#mypage'
+  post '/login' => 'users#login'
+end
